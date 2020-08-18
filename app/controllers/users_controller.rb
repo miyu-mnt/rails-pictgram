bee5c22544d89a029bcfd4c5ce82323c64ddb92a
EC2 Default User <ec2-user@ip-172-31-1-98.us-east-2.compute.internal>
2020-08-18 10:52:32 +0000

    課題commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user=User.new
+  end
+  
+  def create
+    binding.pry
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: '登録が完了しました'
+    else
+      render :new
+      flash.now[:danger] = "登録に失敗しました"
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    
+    validates :name, presence: true ,length: {max: 15}
+    validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+    validates :password, presence: true, length: {in: 8..32}, format: {with: VALID_PASSWORD_REGEX}
+    
+    has_secure_password
+end
