78e60ca6edaa4e4b9e09da0b67804de61103183a
EC2 Default User <ec2-user@ip-172-31-1-98.us-east-2.compute.internal>
2020-08-19 09:06:11 +0000

    課題commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/help'
   
-  resources :users
-  
   get 'sessions/new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
 end
